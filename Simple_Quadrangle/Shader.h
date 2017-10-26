f205f97804984fb16d8820704544b776dbf25ceb
eewano <awanoyasutaka@AwanonoMacBook-puro.local>
2017-10-26 17:26:10 +0900

    コードを整理。

--- Simple_Quadrangle/Shader.h
+++ Simple_Quadrangle/Shader.h
@@ -19,6 +19,7 @@ public:
         std::string fragmentCode;
         std::ifstream vShaderFile;
         std::ifstream fShaderFile;
+
         //例外を飛ばせるよう、ifstreamオブジェクトを確保する
         vShaderFile.exceptions(std::ifstream::badbit);
         fShaderFile.exceptions(std::ifstream::badbit);
@@ -27,21 +28,26 @@ public:
             vShaderFile.open(vertexPath);
             fShaderFile.open(fragmentPath);
             std::stringstream vShaderStream, fShaderStream;
+
             //ファイルのbufferの中身をstreamに読み込む
             vShaderStream << vShaderFile.rdbuf();
             fShaderStream << fShaderFile.rdbuf();
+
             //ファイルをクローズする
             vShaderFile.close();
             fShaderFile.close();
+
             //streamをstringに変換する
             vertexCode = vShaderStream.str();
             fragmentCode = fShaderStream.str();
+
         } catch (std::ifstream::failure e) {
             std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
         }
 
         const GLchar *vShaderCode = vertexCode.c_str();
         const GLchar *fShaderCode = fragmentCode.c_str();
+
         //2: シェーダーの編集
         GLuint vertex, fragment;
         GLint success;
@@ -51,6 +57,7 @@ public:
         vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode, nullptr);
         glCompileShader(vertex);
+
         //コンパイルエラーの出力
         glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
         if (!success) {
@@ -62,6 +69,7 @@ public:
         fragment = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragment, 1, &fShaderCode, nullptr);
         glCompileShader(fragment);
+
         //コンパイルエラーの出力
         glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
         if (!success) {
@@ -74,6 +82,7 @@ public:
         glAttachShader(this->m_Program, vertex);
         glAttachShader(this->m_Program, fragment);
         glLinkProgram(this->m_Program);
+
         //リンクエラーの出力
         glGetProgramiv(this->m_Program, GL_LINK_STATUS, &success);
         if (!success) {
@@ -81,7 +90,7 @@ public:
             std::cout << "ERROR::SHADER::PROGRAM::LINKING_RAILED\n" << infoLog << std::endl;
         }
 
-        //プログラム内にリンクされたので、不要となったシェーダーを削除する
+        //プログラム内にリンクされたので、不要となったシェーダーのデータを削除する
         glDeleteShader(vertex);
         glDeleteShader(fragment);
     }
@@ -91,6 +100,5 @@ public:
         glUseProgram(this->m_Program);
     }
 };
-
 
 #endif /* Shader_h */
